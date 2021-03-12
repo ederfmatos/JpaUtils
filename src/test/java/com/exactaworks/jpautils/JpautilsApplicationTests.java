4e4f6f6199c2a18946213bf1d1b6042bdaa89a74
Eder Matos <ederfmatos@gmail.com>
2021-03-12 18:46:22 -0300

    :green_heart: Add support to gradle package registry

--- src/test/java/com/exactaworks/jpautils/JpautilsApplicationTests.java
+++ src/test/java/com/exactaworks/jpautils/JpautilsApplicationTests.java
@@ -3,7 +3,6 @@ package com.exactaworks.jpautils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class JpautilsApplicationTests {
 
     @Test
