f8980237466d237dc07d312b761cd56543b8f407
Eder Matos <ederfmatos@gmail.com>
2021-03-12 18:21:07 -0300

    :tada: Create project

---
+++ src/test/java/com/exactaworks/jpautils/JpautilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exactaworks.jpautils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpautilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
