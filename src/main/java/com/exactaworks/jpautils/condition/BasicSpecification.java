4f7b151479a5d26c35b36a770ff3dc65eb9186c2
Eder Matos <ederfmatos@gmail.com>
2021-03-12 23:10:37 -0300

    :sparkles: Create Basic Specification

---
+++ src/main/java/com/exactaworks/jpautils/condition/BasicSpecification.java
@@ -0,0 +1,11 @@
+package com.exactaworks.jpautils.condition;
+
+import org.springframework.data.jpa.domain.Specification;
+
+public class BasicSpecification<T> {
+
+    public Specification<T> columnIsEquals(String column, Object value) {
+        return (root, query, builder) -> builder.equal(root.get(column), value);
+    }
+
+}
