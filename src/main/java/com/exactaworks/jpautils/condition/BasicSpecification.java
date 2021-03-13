786c25803b8b22f3a1094af177bb23323091a0bd
Eder Matos <34350634+ederfmatos@users.noreply.github.com>
2021-03-12 23:22:28 -0300

    :bug: Fix BasicSpecification

--- src/main/java/com/exactaworks/jpautils/condition/BasicSpecification.java
+++ src/main/java/com/exactaworks/jpautils/condition/BasicSpecification.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class BasicSpecification<T> {
 
-    public Specification<T> columnIsEquals(String column, Object value) {
+    public static Specification<T> columnIsEquals(String column, Object value) {
         return (root, query, builder) -> builder.equal(root.get(column), value);
     }
 
